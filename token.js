777beef54388728d74ca8249fea21833335d423f
enya-niya <cy24249@gmail.com>
2020-09-02 18:18:08 +0800

    版本3

--- token.js
+++ token.js
@@ -1,1 +1,4 @@
-let token="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlSWQiOiIxIiwiaXNzIjoiaXNzdWVyIiwicm9sZU5hbWUiOiJhZG1pbiIsImlkIjoiMSIsImV4cCI6MTU5ODg3OTE3NCwidXNlcm5hbWUiOiJqZXQifQ.qLp2KlzsST_qe9yB8Dj4xH67aCdFWpI-0CiLmkZVH-I"let userId=3;let userName="testZXB";
+//let token="bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlSWQiOiIxIiwiaXNzIjoiaXNzdWVyIiwicm9sZU5hbWUiOiJhZG1pbiIsImlkIjoiMSIsImV4cCI6MTU5OTA3MDUyMywidXNlcm5hbWUiOiJqZXQifQ.bj-j435ortV-bri5P1UL5I07-oXiy5R1XImF9VWfztA"
+let token=""
+let userId;
+let userName;
